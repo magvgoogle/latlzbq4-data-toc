9ffb2187b856ca933dda3fdc7540c4b0530a4092
Mauricio Alejandro Garcia Valdez <mauricio.garcia@autonomic.ai>
2023-02-13 13:22:54 -0600

    First Commit

---
+++ foundations/log-export.tf
@@ -0,0 +1,21 @@
+module "logsink-423202022682-logbucketsink" {
+  source  = "terraform-google-modules/log-export/google"
+  version = "~> 7.3.0"
+
+  destination_uri      = module.tocdata-joonix-logging-destination.destination_uri
+  log_sink_name        = "423202022682-logbucketsink"
+  parent_resource_id   = var.org_id
+  parent_resource_type = "organization"
+  include_children     = true
+}
+
+module "tocdata-joonix-logging-destination" {
+  source  = "terraform-google-modules/log-export/google//modules/logbucket"
+  version = "~> 7.4.1"
+
+  project_id               = module.logging-jq374-lq533.project_id
+  name                     = "tocdata-joonix-logging"
+  location                 = "global"
+  retention_days           = 365
+  log_sink_writer_identity = module.logsink-423202022682-logbucketsink.writer_identity
+}
---
+++ foundations/projects.tf
@@ -0,0 +1,86 @@
+module "logging-jq374-lq533" {
+  source  = "terraform-google-modules/project-factory/google"
+  version = "~> 12.0"
+
+  name       = "logging"
+  project_id = "logging-jq374-lq533"
+  org_id     = var.org_id
+  folder_id  = google_folder.common.name
+
+  billing_account = var.billing_account
+}
+
+module "monitoring-dev-jq374-lq533" {
+  source  = "terraform-google-modules/project-factory/google"
+  version = "~> 12.0"
+
+  name       = "monitoring-dev"
+  project_id = "monitoring-dev-jq374-lq533"
+  org_id     = var.org_id
+  folder_id  = google_folder.common.name
+
+  billing_account = var.billing_account
+}
+
+module "monitoring-nonprod-jq374-lq533" {
+  source  = "terraform-google-modules/project-factory/google"
+  version = "~> 12.0"
+
+  name       = "monitoring-nonprod"
+  project_id = "monitoring-nonprod-jq374-lq533"
+  org_id     = var.org_id
+  folder_id  = google_folder.common.name
+
+  billing_account = var.billing_account
+}
+
+module "monitoring-prod-jq374-lq533" {
+  source  = "terraform-google-modules/project-factory/google"
+  version = "~> 12.0"
+
+  name       = "monitoring-prod"
+  project_id = "monitoring-prod-jq374-lq533"
+  org_id     = var.org_id
+  folder_id  = google_folder.common.name
+
+  billing_account = var.billing_account
+}
+
+module "vpc-host-dev-jq374-lq533" {
+  source  = "terraform-google-modules/project-factory/google"
+  version = "~> 12.0"
+
+  name       = "vpc-host-dev"
+  project_id = "vpc-host-dev-jq374-lq533"
+  org_id     = var.org_id
+  folder_id  = google_folder.common.name
+
+  enable_shared_vpc_host_project = true
+  billing_account = var.billing_account
+}
+
+module "vpc-host-nonprod-jq374-lq533" {
+  source  = "terraform-google-modules/project-factory/google"
+  version = "~> 12.0"
+
+  name       = "vpc-host-nonprod"
+  project_id = "vpc-host-nonprod-jq374-lq533"
+  org_id     = var.org_id
+  folder_id  = google_folder.common.name
+
+  enable_shared_vpc_host_project = true
+  billing_account = var.billing_account
+}
+
+module "vpc-host-prod-jq374-lq533" {
+  source  = "terraform-google-modules/project-factory/google"
+  version = "~> 12.0"
+
+  name       = "vpc-host-prod"
+  project_id = "vpc-host-prod-jq374-lq533"
+  org_id     = var.org_id
+  folder_id  = google_folder.common.name
+
+  enable_shared_vpc_host_project = true
+  billing_account = var.billing_account
+}
